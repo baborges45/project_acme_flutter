b6b17744d9cf534b2bb0451fd6869c19a18f020d
bruno.dev <brunoborges.dev45@gmail.com>
2022-09-30 16:37:58 -0300

    product screen

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -24,6 +24,28 @@ class ProductScreen extends StatelessWidget {
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(50),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade600,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      itemModel.itemName!,
+                      maxLines: 2,
+                      style: TextStyle(
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
